4c3477af5b5862edc27b2f15392c63212867b032
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-06-26 20:00:36 -0400

    feat: ajuste no schema

--- prisma/generated/client/index.d.ts
+++ prisma/generated/client/index.d.ts
@@ -355,8 +355,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.6.0
-   * Query Engine version: f676762280b54cd07c770017ed3711ddde35f37a
+   * Prisma Client JS version: 6.10.1
+   * Query Engine version: 9b628578b3b7cae625e8c927178f15a170e74a9c
    */
   export type PrismaVersion = {
     client: string
