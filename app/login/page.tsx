0ba6157296785baecd227ecad0088e43f2421b60
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-07-02 20:36:05 -0400

    style: adjustment in Saas styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ const LoginPage = async () => {
   return (
     <div className="grid min-h-screen grid-cols-2 bg-gradient-to-br from-[#0C0A09] to-[#0C0A09]">
       <div className="max-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <div className="absolute z-20 w-auto rounded-lg bg-white bg-opacity-5 p-8 backdrop-blur-sm sm:relative sm:bg-transparent sm:p-0">
+        <div className="absolute z-20 w-auto rounded-lg bg-black bg-opacity-40 p-8 backdrop-blur-sm sm:relative sm:bg-transparent sm:p-0">
           <Image
             src="/logo.png"
             width={140}
@@ -24,7 +24,7 @@ const LoginPage = async () => {
           <h1 className="mb-4 text-3xl font-semibold tracking-tight text-white">
             Bem-vindo ao MyFinManager
           </h1>
-          <p className="mb-8 text-lg leading-relaxed text-gray-300">
+          <p className="text-shadow-md mb-8 text-lg leading-relaxed text-white sm:text-gray-300">
             Uma plataforma de gestão financeira com IA para monitorar suas
             movimentações, oferecer insights personalizados e simplificar o
             controle do seu orçamento.
